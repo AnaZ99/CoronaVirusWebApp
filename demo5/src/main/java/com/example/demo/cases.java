9b0bff475d5d01bb32d6a7dc0b3ec384ebfbb426
VaneS2 <74139708+VaneS2@users.noreply.github.com>
2022-02-25 02:27:24 +0100

    s
    s

---
+++ demo5/src/main/java/com/example/demo/cases.java
@@ -0,0 +1,91 @@
+package com.example.demo;
+
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Controller
+public class cases {
+    HttpURLConnection connection;
+    @GetMapping("/cases")
+    public String prikaz(Model model) {
+
+
+
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContet = new StringBuffer();
+
+        try{
+            URL url =new URL("https://api.covid19api.com/summary");
+            connection=(HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            int status=connection.getResponseCode();
+            if(status>299){
+                reader=new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while((line=reader.readLine())!=null){
+                    responseContet.append(line);
+                }
+                reader.close();
+            }
+            else{
+                reader=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while((line=reader.readLine())!=null){
+                    responseContet.append(line);
+                    break;
+                }
+                reader.close();
+
+            }
+            parse(responseContet.toString());
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
+
+return "cases.html";
+    }
+
+
+    public static  void parse (String responsebody) throws JSONException {
+        JSONObject albums= new JSONObject(responsebody);
+        JSONArray zemji= albums.getJSONArray("Countries");
+
+        for(int i=0;i<zemji.length();i++)
+        {
+            JSONObject zemja=zemji.getJSONObject(i);
+
+            if(zemja.getString("Country").equals("Albania"))
+            {
+                System.out.println(zemja.getString("Country"));
+            }
+
+
+        }
+
+    }
+
+
+
+}
---
+++ demo5/src/main/java/com/example/demo/events.java
@@ -0,0 +1,207 @@
+package com.example.demo;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.bigquery.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Controller
+public class events {
+    @GetMapping({"/events",""})
+    public String gett(Model model)
+    {
+        String query = "SELECT * \n" +
+                " FROM `gdelt-bq.gdeltv2.ggg` where \n" +
+                " (ContextualText like '%covid%' and ContextualText like '%coronavirus%')and GeoType>1\n" +
+                "  and DATE(DateTime) >= \""+java.time.LocalDate.now().minusDays(2)+"\"  ";
+
+
+        try {
+            // Initialize client that will be used to send requests. This client only needs to be created
+            // once, and can be reused for multiple requests.
+
+            String jsonPath="../universal-chain-342203-af8ccc9a3556.json";
+            GoogleCredentials credentials= null;
+            try {
+                credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            BigQuery bigquery = BigQueryOptions.newBuilder()
+                    .setCredentials(credentials)
+                    .build().getService();
+
+
+            QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).build();
+
+
+            TableResult result = bigquery.query(queryConfig);
+
+            ArrayList<CoronaEvents> list= new ArrayList<>();
+
+            for (FieldValueList row : bigquery.query(queryConfig).iterateAll()) {
+                String url=row.get(1).getValue().toString();
+
+                String title="";
+                if(row.get(2).getValue()==null)
+                {
+                    title="";
+                }
+                else
+                {
+                    title=row.get(2).getValue().toString();
+                }
+                String image="";
+                if(row.get(3).getValue()==null)
+                {
+                    image="";
+                }
+                else
+                {
+                    image=row.get(3).getValue().toString();
+                }
+
+                String location=row.get(7).getValue().toString();
+                String lat= row.get(8).getValue().toString();
+                String lon= row.get(9).getValue().toString();
+                String countryCode=row.get(10).getValue().toString();
+                String contextualText=row.get(14).getValue().toString();
+
+                list.add(new CoronaEvents(url,title,image,location,lat,lon,countryCode,contextualText));
+
+            }
+
+
+            if(list.size()==0)
+            {
+                model.addAttribute("nemanisto",1);
+            }
+            else
+            {
+                model.addAttribute("nemanisto",2);
+            }
+            model.addAttribute("nastani",list);
+
+
+
+
+            System.out.println("Query ran successfully");
+        } catch (BigQueryException | InterruptedException e) {
+            System.out.println("Query did not run \n" + e.toString());
+        }
+
+        return "map.html";
+    }
+
+
+@GetMapping("/search")
+    public String pokategorija(Model model, HttpServletRequest request) {
+
+        String datum = request.getParameter("date");
+        String []datumm=datum.split("/");
+        String   finalDate= datumm[2] + "-" + datumm[0] + "-" + datumm[1];
+
+    String lokacija = request.getParameter("search");
+
+        String query = "SELECT * \n" +
+                " FROM `gdelt-bq.gdeltv2.ggg` where \n" +
+                " (ContextualText like '%covid%' and ContextualText like '%coronavirus%')and GeoType>1\n" +
+                " and Location like '%" + lokacija + "' "+
+                "  and DATE(DateTime) = \""+finalDate+"\"  ";
+
+
+        System.out.println(query);
+
+  //  and DATE(DateTime) = \""+d+"\"  ";
+ //   "  and DATE(DateTime) = \"2021-10-01\"  ";
+
+        try {
+            // Initialize client that will be used to send requests. This client only needs to be created
+            // once, and can be reused for multiple requests.
+
+            String jsonPath="../universal-chain-342203-af8ccc9a3556.json";
+            GoogleCredentials credentials= null;
+            try {
+                credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            BigQuery bigquery = BigQueryOptions.newBuilder()
+                    .setCredentials(credentials)
+                    .build().getService();
+
+            // Create the query job.
+            QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).build();
+
+            // Execute the query.
+            TableResult result = bigquery.query(queryConfig);
+
+            ArrayList<CoronaEvents> list= new ArrayList<>();
+            //  FieldValueList row = result.getValues().iterator().next();
+            for (FieldValueList row : bigquery.query(queryConfig).iterateAll()) {
+                //System.out.println(row.get(1).getValue().toString());
+                String url=row.get(1).getValue().toString();
+
+                String title="";
+                if(row.get(2).getValue()==null)
+                {
+                    title="";
+                }
+                else
+                {
+                    title=row.get(2).getValue().toString();
+                }
+                String image="";
+                if(row.get(3).getValue()==null)
+                {
+                    image="";
+                }
+                else
+                {
+                    image=row.get(3).getValue().toString();
+                }
+
+                String location=row.get(7).getValue().toString();
+                String lat= row.get(8).getValue().toString();
+                String lon= row.get(9).getValue().toString();
+                String countryCode=row.get(10).getValue().toString();
+                String contextualText=row.get(14).getValue().toString();
+
+                list.add(new CoronaEvents(url,title,image,location,lat,lon,countryCode,contextualText));
+
+
+            }
+
+            if(list.size()==0)
+            {
+                model.addAttribute("nemanisto",1);
+            }
+            else
+            {
+                model.addAttribute("nemanisto",2);
+            }
+            model.addAttribute("nastani",list);
+
+
+
+
+            System.out.println("Query ran successfully");
+        } catch (BigQueryException | InterruptedException e) {
+            model.addAttribute("nemanisto",1);
+            System.out.println("Query did not run \n" + e.toString());
+        }
+
+        return "map.html";
+
+
+    }
+}
